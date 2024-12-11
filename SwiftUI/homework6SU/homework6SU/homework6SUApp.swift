c249a72c07997d1e116e9699f7453eab4aef9075
eugene-vlasov <eugene.vlasov17@gmail.com>
2024-12-11 08:23:21 +0200

    Added empty projects

---
+++ SwiftUI/homework6SU/homework6SU/homework6SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  homework6SUApp.swift
+//  homework6SU
+//
+//  Created by Evgene Vlasov on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct homework6SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
